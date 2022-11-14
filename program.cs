afa20edb25e784bece78cf66223f22c8602389c7
Ainis Taranda <taranda.ainis@gmail.com>
2022-11-14 15:04:03 -0500

    bank done

--- program.cs
+++ program.cs
@@ -1,4 +1,16 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Welcome to the Bank"); // log in C#
+
+BankAccount account = new BankAccount(0); // ('type' 'variable name' = 'creating new, instantiates' 'function' )
+
+// account.balance = 100;
+try {
+  account.makeDeposit(-2);
+} catch(Exception ex){
+  Console.WriteLine(ex.Message);
+}
+
+Console.Write($"your balance is: {account.getBalance()}");
+Console.WriteLine($" Account Number: {account.accountNumber}");
 
 
 
